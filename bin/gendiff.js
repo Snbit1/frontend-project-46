9b20bc3f0192bde3a7b2f033b448dbcdbda1b45d
Александр <k.sasha.228228@gmail.com>
2025-06-01 15:13:26 +0300

    Changed gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,23 +7,10 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-
-program
+  .version('0.0.1')
   .argument('<filepath1>', 'path to the first file')
   .argument('<filepath2>', 'path to the second file')
   .option('-f, --format <type>', 'output format', 'stylish');
-
-program.helpInformation = () => `
-Usage: gendiff [options] <filepath1> <filepath2>
-
-Compares two configuration files and shows a difference.
-
-Options:
-  -V, --version        output the version number
-  -f, --format <type>  output format
-  -h, --help           display help for command
-`;
 
 program.action((filepath1, filepath2, options) => {
   try {
@@ -33,7 +20,6 @@ program.action((filepath1, filepath2, options) => {
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Error reading or parsing files:', error);
-    process.exit(1);
   }
 });
 
